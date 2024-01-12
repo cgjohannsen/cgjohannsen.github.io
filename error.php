bd4bca12c0aa4c8be0d23038353b448840ba80e0
cgjohannsen <chrisjohannsen8@gmail.com>
2024-01-11 23:32:08 -0600

    adding initial site

---
+++ error.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<title>Chris Johannsen</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+<?php include 'headerbar.html'?>
+
+<div class="main">
+
+<h3>Uh oh, looks like this page doesn't exist.</h3>
+
+</div>
+
+</body>
+</html>
